56b92fad0c50031af8e230683518a63d31c16abf
Saida Davis <saihdavis@gmail.com>
2020-04-05 17:57:09 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,8 @@ epic_tragedy = {
   }
 
 
-  epic_tragedy = [:montague][:hero][:status] = "dead"
-  epic_tragedy = [:capulet][:heroine][:status] = "dead"
+  epic_tragedy [:montague][:hero][:status] = "dead"
+  epic_tragedy [:capulet][:heroine][:status] = "dead"
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
