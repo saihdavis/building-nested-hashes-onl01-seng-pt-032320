cfc93458ae0c553371615a21145ab3a6b03ea4e3
Saida Davis <saihdavis@gmail.com>
2020-04-05 17:47:28 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,10 +21,10 @@ def bonus
    }
   }
 
-  #code your solution here:
 
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
+end
 end
